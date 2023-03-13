912685a895e2376fc568ab4bc5a58927701a81b2
Jinghui Liao <jinghui@wayne.edu>
2023-03-13 17:24:07 -0400

    test

--- neo-cli/CLI/MainService.cs
+++ neo-cli/CLI/MainService.cs
@@ -257,17 +257,17 @@ namespace Neo.CLI
             // Basic script checks
 
             Script script = new Script(nef.Script);
-            for (var i = 0; i < script.Length;)
-            {
-                // Check bad opcodes
-
-                // Instruction inst = script.GetInstruction(i);
-                // if (inst is null || !Enum.IsDefined(typeof(OpCode), inst.OpCode))
-                // {
-                //     throw new FormatException($"OpCode not found at {i}-{((byte)inst.OpCode).ToString("x2")}");
-                // }
-                // i += inst.Size;
-            }
+            // for (var i = 0; i < script.Length;)
+            // {
+            //     // Check bad opcodes
+            //
+            //     // Instruction inst = script.GetInstruction(i);
+            //     // if (inst is null || !Enum.IsDefined(typeof(OpCode), inst.OpCode))
+            //     // {
+            //     //     throw new FormatException($"OpCode not found at {i}-{((byte)inst.OpCode).ToString("x2")}");
+            //     // }
+            //     // i += inst.Size;
+            // }
 
             // Build script
 
