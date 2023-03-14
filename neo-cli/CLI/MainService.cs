4a02116397003142e1e1657845ddcf360aaf34fe
Jinghui Liao <jinghui@wayne.edu>
2023-03-14 09:24:54 -0400

    test

--- neo-cli/CLI/MainService.cs
+++ neo-cli/CLI/MainService.cs
@@ -311,7 +311,7 @@ namespace Neo.CLI
 
             // Basic script checks
 
-            Script script = new Script(nef.Script);
+            nef.Script.IsScriptValid(manifest.Abi);
             // for (var i = 0; i < script.Length;)
             // {
             //     // Check bad opcodes
